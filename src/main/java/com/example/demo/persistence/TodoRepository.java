10d5d5851a859de1758930b9aff4851016b9a5b0
nrj022 <0602smj@gmail.com>
2023-06-19 10:55:32 +0900

    백엔드 개발 16. @Repository 사용하기

---
+++ src/main/java/com/example/demo/persistence/TodoRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.demo.model.*;
+
+public interface TodoRepository extends JpaRepository<TodoEntity, String>{
+
+}
